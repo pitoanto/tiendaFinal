290c78d1280187719b0623addcae00bfd787fa45
Antonio <antoni7070@gmail.com>
2022-06-07 20:05:05 +0200

    next

--- store.php
+++ store.php
@@ -1,23 +1,23 @@
 <?php
 include "web/header.php";
-
 $mysqli = new mysqli("localhost", "root", "", "raperos");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-
-$consulta = "SELECT * FROM rapero ORDER BY RAND() LIMIT 10";
-
-if ($resultado = $mysqli->query($consulta)) {
-    while ($fila = $resultado->fetch_assoc()) {
-        include 'web/rapero.html';
-    }
-}
 ?>
+
 <div class="container-fluid">
     <div class="row">
         <div class="col">
 
+            <?php
+            $consulta = "SELECT * FROM raperos ORDER BY RAND() LIMIT 10";
+            if ($resultado = $mysqli->query($consulta)) {
+                while ($fila = $resultado->fetch_assoc()) {
+                    include 'web/rapero.html';
+                }
+            };
+            ?>
         </div>
     </div>
 </div>
