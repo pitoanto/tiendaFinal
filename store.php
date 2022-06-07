f4dadf195e3d29baa1524cfaafa8f6ad9bef1dfb
Antonio <antoni7070@gmail.com>
2022-06-07 18:40:10 +0200

    comienzo tienda

--- store.php
+++ store.php
@@ -1,3 +1,13 @@
 <?php
 include "web/header.php";
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+
+        </div>
+    </div>
+</div>
+<?php
 include "web/footer.php";
+?>
